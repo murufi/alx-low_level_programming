75be02a4428a8bd4ceab00658a4619119cee3c71
AhmadRufi <rufaiabubakar23@gmail.com>
2023-09-10 22:42:30 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  Entery point
+ * Discripton: 'Check for number if it is positive or negative'
+ * Return: 'always 0'
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 0; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}

46864482d648cd98ec977077f494bc02054bcabe
AhmadRufi <rufaiabubakar23@gmail.com>
2023-09-10 21:49:10 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -  Entery point
+ * Discripton: 'Check for number if it is positive or negative'
+ * Return: 'always 0'
+ */
+int main(void)
+{
+    putchar(65);
+
+    return (0);
+}

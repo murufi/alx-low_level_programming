3442d0d29b12cc404931c1da56498448b0d996eb
AhmadRufi <rufaiabubakar23@gmail.com>
2023-09-10 23:10:13 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  Entery point
+ * Discripton: 'Check for number if it is positive or negative'
+ * Return: 'always 0'
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}

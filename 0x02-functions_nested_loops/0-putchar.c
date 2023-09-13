ab321b08b12edd0ee6b6d823989e252754509373
AhmadRufi <rufaiabubakar23@gmail.com>
2023-09-12 20:28:13 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main -  Entery point
+ * Discripton: 'Check for number if it is positive or negative'
+ * Return: 'always 0'
+ */
+int _putchar(char c)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+	return (0);
+}

ada4d08a652c8efb79a660e0a1c67b297bf00b7e
AhmadRufi <rufaiabubakar23@gmail.com>
2023-09-12 21:05:36 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -  Entery point
+ * Discripton: 'Check for number if it is positive or negative'
+ * Return: 'always 0'
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 98))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

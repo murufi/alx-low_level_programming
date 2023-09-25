35d38d318728254120d481389c9c0f3164926758
AhmadRufai <okenegist@gmail.com>
2023-09-25 00:06:30 -0700

    task 003

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * _islower - checks for lower character
+ * @c: The character to be checked
  * Discripton: 'Check for number if it is positive or negative'
  * Return: 'always 0'
  */
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
